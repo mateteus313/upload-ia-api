9060876c6db48239a155eaa167d5425f630a8076
Matheus Santos <mateteus07042000@gmail.com>
2023-09-13 11:53:29 -0300

    newImport-routeRegistration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
+import { fastifyCors } from '@fastify/cors'
+
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello World'
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app.register(fastifyCors, {
+    // Se o projeto for ao ar, inserir o site de forma correta aqui
+    origin: '*'
 })
 
 app.listen({
